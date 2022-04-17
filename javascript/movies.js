6df8b714996951faddde2803a1197446ea53620e
lekshyuen <65160468+lekshyuen@users.noreply.github.com>
2022-04-17 11:45:56 +0800

    haha

---
+++ javascript/movies.js
@@ -0,0 +1,55 @@
+function getMovies() {
+    return [
+        {
+            id:1,
+            type:"now",
+            thumbnail:"casablanca.png",
+            src:"http://courses.cs.cityu.edu.hk/cs2204/video/casablanca-s.mp4",
+            name:"Cassblanca",
+            cast:"Humphrey Bogart, Ingrid Bergmen",
+            director:"Michael Curtiz",
+            duration: 130
+        },
+        {
+            id:2,
+            type:"now",
+            thumbnail:"wildlife.png",
+            src:"http://courses.cs.cityu.edu.hk/cs2204/video/wildlife.mp4",
+            name:"Big Buck",
+            cast:"Big Bunny, Little Fox",
+            director:"Forest King",
+            duration: 100
+        },
+        {
+            id:3,
+            type:"upcoming",
+            thumbnail:"nature.png",
+            src:"http://courses.cs.cityu.edu.hk/cs2204/video/nature.mp4",
+            name:"Nature",
+            cast:"Parrot, birds",
+            director:"Mother Nature",
+            duration: 100
+        },
+		{
+            id:4,
+            type:"now",
+            thumbnail:"casablanca.png",
+            src:"http://courses.cs.cityu.edu.hk/cs2204/video/casablanca-s.mp4",
+            name:"Iron man",
+            cast:"Cast 1, cast 2",
+            director:"Micheal",
+            duration: 130
+        },
+		{
+            id:5,
+            type:"upcoming",
+            thumbnail:"casablanca.png",
+            src:"http://courses.cs.cityu.edu.hk/cs2204/video/casablanca-s.mp4",
+            name:"Pokemon",
+            cast:"fifth movie1, fifth movie2",
+            director:"Derrick",
+            duration: 130
+        },
+		
+    ]
+}
